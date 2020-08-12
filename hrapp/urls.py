4d4be1046506346f7e04d28d3718e5170aa9dcd9
BryanNilsen <bryannilsen@gmail.com>
2020-08-12 00:27:58 -0500

    added login to nav

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -6,7 +6,7 @@ from .views import *
 app_name = 'hrapp'
 urlpatterns = [
     path('', home, name='home'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls'), name='login'),
     path('logout/', logout_user, name='logout'),
     path('employees/', employee_list, name='employee_list'),
     path('employees/form', employee_form, name='employee_form'),
